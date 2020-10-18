92d6a8757633b9686ca13f7afbaa8855a80bce23
Mathieu Delehaye <mathieu.delehaye@gmail.com>
2020-10-18 22:39:53 +0200

    Hashing and salting password with bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
 // const encrypt = require("mongoose-encryption") // 1/ Authentication with encryption key and environment variable
-const md5 = require('md5') // 2/ Authentication with hash function
+// const md5 = require('md5') // 2/ Authentication with hash function
+const bcrypt = require('bcrypt') // 3/ Hashing and salting password
+
+const saltRounds = 10 // 3/ Hashing and salting password
 
 const app = express()
 
@@ -44,17 +47,27 @@ app.route('/register')
   })
 
   .post(function(req, res) {
-    const user = new User({
-      email: req.body.username,
-      // password: req.body.password  // 1/ Authentication with encryption key and environment variable
-      password: md5(req.body.password) // 2/ Authentication with hash function
-    })
-    user.save(function(err) {
-      if (!err) {
-        res.render("secrets")
-      } else {
-        console.log(err)
-      }
+
+    console.log(req.body.password)
+
+    // 3/ Hashing and salting password
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+      console.log(hash)
+
+      const user = new User({
+        email: req.body.username,
+        // password: req.body.password  // 1/ Authentication with encryption key and environment variable
+        // password: md5(req.body.password) // 2/ Authentication with hash function
+        password: hash // 3/ Hashing and salting password
+      })
+      user.save(function(err) {
+        if (!err) {
+          res.render("secrets")
+        } else {
+          console.log(err)
+        }
+      })
     })
   })
 
@@ -65,21 +78,26 @@ app.route('/login')
   })
 
   .post(function(req, res) {
-    const username = req.body.username
-    const password = md5(req.body.password) // 2/ Authentication with hash function
 
-    User.findOne({
-      email: username
-    }, function(err, foundUser) {
+      const username = req.body.username
+      const password = req.body.password
 
-      if (err) {
-        console.log(err)
-      } else {
-        if (foundUser.password === password) {
-          res.render("secrets")
+      User.findOne({
+        email: username
+      }, function(err, foundUser) {
+
+        if (err) {
+          console.log(err)
+        } else {
+          // if (foundUser.password === md5(req.body.password)) { // 2/ Authentication with hash function
+          // 3/ Hashing and salting password
+          bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
+              res.render("secrets")
+            }
+          })
         }
-      }
-    })
+      })
   })
 
 app.route('/logout')
